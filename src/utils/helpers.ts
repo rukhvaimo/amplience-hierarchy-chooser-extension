4a2e7c90605ebe377ab93942c2ca8282d7e6211c
Jonny Cheetham <jcheetham@amplience.com>
2021-01-18 16:03:37 +0000

    Moved detokenize function to helpers file

--- src/utils/helpers.ts
+++ src/utils/helpers.ts
@@ -3,16 +3,18 @@ import {
   concat,
   curry,
   equals,
-  identity,
   isEmpty,
   pipe,
   toString,
   type,
-  unapply,
   when,
   complement,
   addIndex,
   reduce,
+  always,
+  ifElse,
+  isNil,
+  keys,
 } from "ramda";
 
 /**
@@ -58,3 +60,34 @@ export const notEmpty = complement(isEmpty);
  * Reduce with list property
  */
 export const reduceIdx = addIndex(reduce);
+
+function getRegx(prefix: string, suffix: string, variableName: string) {
+  return new RegExp(`${prefix}${variableName}${suffix}`, "g");
+}
+
+export function detokenize(
+  template: string,
+  variables: { [name: string]: string | undefined },
+  tokenPrefix: string = "{{",
+  tokenSuffix: string = "}}"
+): string {
+  //@ts-ignore
+  return pipe(
+    keys,
+    reduce(
+      (tmplt, varName) =>
+        pipe(
+          //@ts-ignore
+          propOr(null, __, variables),
+          ifElse(isNil, always(tmplt), (varValue) =>
+            tmplt.replace(
+              getRegx(tokenPrefix, tokenSuffix, varName),
+              encodeURIComponent(varValue)
+            )
+          )
+          //@ts-ignore
+        )(varName),
+      template
+    )
+  )(variables);
+}
